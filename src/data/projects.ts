015490cdf2456db943e39e494df970eb406ec1a4
LUIS ANGEL ALMAZAN DE JESUS <147682457+LuisAngelAlmazan@users.noreply.github.com>
2025-08-08 13:21:49 -0600

    tarjetas

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -7,6 +7,28 @@ export const projects = [
     projectLink: 'https://airesacondicionados.netlify.app/', 
   },
   {
+    title: 'Rental Biker | Tour en Bicicleta',
+    description: 'Sitio web responsivo para alquiler de bicicletas en Los Cabos, con soporte multilenguaje y rutas interactivas.',
+    tecnologias: ['React', 'TypeScript', 'Tailwind CSS', 'Framer Motion', 'i18next'],
+    imageUrl: '/rentalbike.png',
+    projectLink: 'https://rentalbiker.netlify.app/',
+  },
+  {
+    title: 'Plataforma de Rifas (En desarrollo)',
+    description: 'Sistema escalable de rifas con frontend atractivo y planes por suscripción. Incluye reserva de boletos, panel admin y automatización.',
+    tecnologias: ['Next.js', 'Tailwind CSS', 'TypeScript', 'Node.js (futuro)'],
+    imageUrl: '/rifaspro.png',
+    projectLink: 'https://rifaspro.netlify.app/',
+  },
+  {
+  title: "Cacao",
+  description:
+    "Landing page enfocada en productos naturales y artesanales, con pedidos directos por WhatsApp. Presenta la historia, beneficios y testimonios para conectar con el cliente y facilitar la conversión.",
+  tecnologias: ["React", "Tailwind", "WhatsApp API", "UX/UI", "Diseño Responsive"],
+  projectLink: "https://cacao.example.com", 
+  imageUrl: "/images/projects/cacao.webp" 
+},
+{
     title: 'Guitar LA',
     description: 'Carrito de compras de guitarras con interfaz intuitiva.',
     tecnologias: ['React', 'JavaScript', 'Bootstrap', 'CSS'],
@@ -20,18 +42,4 @@ export const projects = [
     imageUrl: '/Ecomerce.png',
     projectLink: 'https://github.com/LuisAngelAlmazan/ecommerce-backend',
   },
-  {
-    title: 'Rental Biker | Tour en Bicicleta',
-    description: 'Sitio web responsivo para alquiler de bicicletas en Los Cabos, con soporte multilenguaje y rutas interactivas.',
-    tecnologias: ['React', 'TypeScript', 'Tailwind CSS', 'Framer Motion', 'i18next'],
-    imageUrl: '/rentalbike.png',
-    projectLink: 'https://rentalbiker.netlify.app/',
-  },
-  {
-    title: 'Plataforma de Rifas (En desarrollo)',
-    description: 'Sistema escalable de rifas con frontend atractivo y planes por suscripción. Incluye reserva de boletos, panel admin y automatización.',
-    tecnologias: ['Next.js', 'Tailwind CSS', 'TypeScript', 'Node.js (futuro)'],
-    imageUrl: '/rifaspro.png',
-    projectLink: 'https://rifaspro.netlify.app/',
-  }
 ];
